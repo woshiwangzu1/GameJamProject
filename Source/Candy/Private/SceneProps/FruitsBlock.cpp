14dd94f12fc00f8ab3796080887cf3beec19264a
senkeer <552152117@qq.com>
2022-07-01 21:56:51 +0800

    Addddddddddd

---
+++ Source/Candy/Private/SceneProps/FruitsBlock.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SceneProps/FruitsBlock.h"
+
+// Sets default values
+AFruitsBlock::AFruitsBlock()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFruitsBlock::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFruitsBlock::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
