931d960fb40169feb498c25e2a5541f94a43baa3
sunlishan <1214994391@qq.com>
2022-07-02 11:36:52 +0800

    add

--- Source/Candy/Private/SceneProps/HoneycombActor.cpp
+++ Source/Candy/Private/SceneProps/HoneycombActor.cpp
@@ -39,6 +39,7 @@ void AHoneycombActor::UpdateBoxAnimation(float DeltaTime)
 void AHoneycombActor::OnComponentBeginOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	
 }
 
 // Called every frame
